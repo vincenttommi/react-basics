cdc55717aba7e2d04b6d5393970910cd065c8ef7
vincenttommi <vincenttommi@gmail.com>
2022-07-25 07:14:54 +0300

    class components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import React, { Component } from 'react';
+class App extends React.Component {
+        constructor() {
+            super();
+            this.state = {
+                data: [{
+                        "name": "vincent"
+                    },
+                    {
+                        "name": "tommi"
+                    },
+                    {
+                        "name": "lilian"
+                    }
+                ]
+            }
+        }
+        render() {
+                return ( <
+                    div >
+                    <
+                    StudentName / >
+                    <
+                    ul > {
+                        this.state.data.map((item) => < List data = { item }
+                            />)}            < /
+                            ul > <
+                            /div>  
+                        );
+                    }
+                }
+                class StudentName extends React.Component {
+                    render() {
+                        return ( <
+                            div >
+                            <
+                            h1 > Student Name Detail < /h1>   < /
+                            div >
+                        );
+                    }
+                }
+                class List extends React.Component {
+                    render() {
+                        return ( <
+                            ul >
+                            <
+                            li > { this.props.data.name } < /li>    < /
+                            ul >
+                        );
+                    }
+                }
+                export default App;
